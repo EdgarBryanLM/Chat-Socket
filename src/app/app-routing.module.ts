94a4bb0c613eb72b1b07d91a30d87bbafa3bb940
EdgarBryanLM <s17120227@alumnos.itsur.edu.mx>
2022-06-18 11:54:27 -0500

    aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioGuardGuard } from './guards/usuario-guard.guard';
+import { LoginComponent } from './pages/login/login.component';
+import { MensajesComponent } from './pages/mensajes/mensajes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+      path:'login',component:LoginComponent
+  },
+  {
+      path:'mensajes',component:MensajesComponent,canActivate:[UsuarioGuardGuard]
+  },
+  {
+    path:'**',
+    redirectTo:'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
